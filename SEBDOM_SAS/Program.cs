690623c1cfbb66aa2c600aec5241d5060ff70951
Santty111 <137450318+Santty111@users.noreply.github.com>
2026-04-05 15:15:23 -0500

    Agregar modulo Pulpos: modelo, migracion, controlador y vistas

--- SEBDOM_SAS/Program.cs
+++ SEBDOM_SAS/Program.cs
@@ -29,6 +29,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Pulpos}/{action=Index}/{id?}");
+
 app.Run();
 
 app.MapControllerRoute(
